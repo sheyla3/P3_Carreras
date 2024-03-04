7894718a9d44d3f816c799b04019d7225bb933db
mohacr026-Portatil <mohamedcampusnet@gmail.com>
2024-03-04 15:38:46 +0100

    act

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Caballos</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
     </head>
     <body>
+    <header>
+        aa
+    </header>
+    
         <h1>Home</h1>
         <a href="{{ route('loginAdmin') }}">Admin</a>
     </body>

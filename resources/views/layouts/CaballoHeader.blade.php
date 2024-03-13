6ba74b5e70d710819cd5ec29efa3a5454231663b
sheyla3 <144891028+sheyla3@users.noreply.github.com>
2024-03-13 09:52:17 +0100

    layouts

---
+++ resources/views/layouts/CaballoHeader.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Caballos</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar bg-body-tertiary p-0" id="navbar">
+        <div class="p-0">
+            <img src="{{ asset('img/logoCaballo.png') }}" alt="Caballo">
+        </div>
+        <div class="right-links">
+            <a href="{{ route('loginAdmin') }}">Iniciar Sesion</a>
+            <a href="{{ route('loginAdmin') }}">Registrarme</a>
+        </div>
+    </nav>
+    <!-- Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>

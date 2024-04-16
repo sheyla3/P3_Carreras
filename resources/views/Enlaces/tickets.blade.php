e630240af5de7964c0046cede2cb2b1425b0769b
mohacr026-Portatil <mohamedcampusnet@gmail.com>
2024-04-16 10:10:03 +0200

    act

--- resources/views/Enlaces/tickets.blade.php
+++ resources/views/Enlaces/tickets.blade.php
@@ -106,6 +106,63 @@
             cursor: pointer;
         }
     </style>
+
+<style>
+        .piece {
+        width: 150px;
+        height: 150px;
+        cursor: pointer;
+        margin-left: 6px;
+        margin-top: 6px;
+        position: absolute;
+        background-color: red;
+        background-size: cover;
+    }
+    .drop-zone {
+      width: 150px;
+      height: 150px;
+      padding: 5px;
+      border: 1px dashed #000;
+      position: absolute;
+    }
+
+    .row1 {
+      top: 0;
+    }
+
+    .row2 {
+      top: 155px;
+    }
+
+    .row3 {
+      top: 310px;
+    }
+
+    .col1 {
+      left: 0;
+    }
+
+    .col2 {
+      left: 155px;
+    }
+
+    .col3 {
+      left: 310px;
+      
+    }
+
+    .container-drak-and-drop {
+        position: absolute;
+        top: 25%;
+        left: 30%;
+        width: 700px;
+        height: 500px;
+        border: 5px black;
+        background: rgb(255, 255, 255);
+        box-shadow: 10px 5px 10px rgb(0 0 0 / 25%);
+
+    }
+</style>
 </head>
 
 <body>
@@ -298,17 +355,29 @@
         // Guardar datos de la carrera y factura
         var carreraId, PrecioTotal, cantidad;
         // Cuando se hace clic en el botón, abrir el modal de detalles
-        for (var i = 0; i < btns.length; i++) {
-            btns[i].onclick = function() {
-                var title = this.getAttribute('data-title');
-                var description = this.getAttribute('data-description');
-                var id = this.getAttribute('data-id');
-                var price = parseFloat(this.getAttribute('data-price')); // Convertir el precio a un número flotante
-                originalPrice = price; // Guardar el precio original de la carrera
-                carreraId = id; // darle el valor del id
-                showDetailsModal(title, description, price);
-            }
-        }
+// Cuando se hace clic en el botón "Comprar", mostrar el área de "Drag and Drop"
+for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function() {
+        var title = this.getAttribute('data-title');
+        var description = this.getAttribute('data-description');
+        var id = this.getAttribute('data-id');
+        var price = parseFloat(this.getAttribute('data-price')); // Convertir el precio a un número flotante
+        originalPrice = price; // Guardar el precio original de la carrera
+        carreraId = id; // darle el valor del id
+        // Ocultar el modal de detalles
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+        // Mostrar el área de "Drag and Drop"
+        var dragAndDropContainer = document.getElementById("dragAndDropContainer");
+        dragAndDropContainer.style.display = "block";
+        // Actualizar el título y descripción si es necesario
+        document.getElementById("modal-title").innerText = title;
+        document.getElementById("modal-description").innerText = description;
+        document.getElementById("modal-price").innerText = price + " €";
+        calculateTotalPrice();
+    }
+}
+
 
         // PayPal Integration
         paypal.Buttons({
@@ -339,6 +408,122 @@
             }
         }).render('#paypal-button-container');
     </script>
+
+
+
+
+<div id="dragAndDropContainer" class="container-drak-and-drop" style="display: none;">
+    <div class="piezas">
+        <div draggable="true" class="piece" id="piece1" style="top: 0; left: 500px; background-image: url('{{ asset('img/9.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece5" style="top: 0; left: 500px; background-image: url('{{ asset('img/5.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece3" style="top: 0; left: 500px; background-image: url('{{ asset('img/7.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece9" style="top: 0; left: 500px; background-image: url('{{ asset('img/1.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece4" style="top: 0; left: 500px; background-image: url('{{ asset('img/6.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece7" style="top: 0; left: 500px; background-image: url('{{ asset('img/3.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece2" style="top: 0; left: 500px; background-image: url('{{ asset('img/8.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece6" style="top: 0; left: 500px; background-image: url('{{ asset('img/4.jpg') }}');"></div>
+        <div draggable="true" class="piece" id="piece8" style="top: 0; left: 500px; background-image: url('{{ asset('img/2.jpg') }}');"></div>
+        <button id="continueButton" style="position: absolute; top: 400px; left: 535px;">Continuar</button>
+
+    </div>
+
+    <div class="drop-zone row1 col1" data-position="1"></div>
+    <div class="drop-zone row1 col2" data-position="2"></div>
+    <div class="drop-zone row1 col3" data-position="3"></div>
+    <div class="drop-zone row2 col1" data-position="4"></div>
+    <div class="drop-zone row2 col2" data-position="5"></div>
+    <div class="drop-zone row2 col3" data-position="6"></div>
+    <div class="drop-zone row3 col1" data-position="7"></div>
+    <div class="drop-zone row3 col2" data-position="8"></div>
+    <div class="drop-zone row3 col3" data-position="9"></div>
+</div>
+
+  <script>
+    function handleDragStart(e) {
+      e.dataTransfer.setData("text/plain", "Heu deixat caure quelcom!");
+      e.dataTransfer.setData("text", e.target.id);
+    }
+
+    function handleDragOver(e) {
+      e.preventDefault();
+    }
+
+    function handleDrop(e) {
+  e.preventDefault();
+  const draggedPieceId = e.dataTransfer.getData("text");
+  const draggedPiece = document.getElementById(draggedPieceId);
+  const dropZone = e.target;
+
+  if (dropZone.classList.contains('drop-zone')) {
+    const position = dropZone.getAttribute('data-position');
+    const pieceWidth = draggedPiece.offsetWidth;
+    const pieceHeight = draggedPiece.offsetHeight;
+
+    // Obtener la posición de la gota
+    const dropPosition = parseInt(dropZone.getAttribute('data-position'));
+
+    // Obtener la posición de la pieza
+    const piecePosition = parseInt(draggedPieceId.replace('piece', ''));
+
+    // Verificar si la posición de la gota y la pieza coinciden
+    if (dropPosition === piecePosition) {
+      // Calcular la posición dentro del cuadrado de la zona de destino
+      const offsetX = e.offsetX;
+      const offsetY = e.offsetY;
+
+      // Ajustar la posición para que la pieza esté centrada en el cuadrado
+      const clampedX = Math.min(Math.max(offsetX - pieceWidth / 2, 0), dropZone.offsetWidth - pieceWidth);
+      const clampedY = Math.min(Math.max(offsetY - pieceHeight / 2, 0), dropZone.offsetHeight - pieceHeight);
+
+      // Establecer la posición de la pieza dentro del cuadrado de la zona de destino
+      draggedPiece.style.top = clampedY + 'px';
+      draggedPiece.style.left = clampedX + 'px';
+
+      dropZone.appendChild(draggedPiece);
+    }
+  }
+}
+
+
+    const pieces = document.querySelectorAll('.piece');
+    pieces.forEach(piece => {
+      piece.addEventListener('dragstart', handleDragStart);
+    });
+
+    const dropZones = document.querySelectorAll('.drop-zone');
+    dropZones.forEach(zone => {
+      zone.addEventListener('dragover', handleDragOver);
+      zone.addEventListener('drop', handleDrop);
+    });
+
+// Manejar el evento de clic en el botón "Continuar"
+continueButton.addEventListener('click', function() {
+    var allImagesPlaced = true;
+    var dropZones = document.querySelectorAll('.drop-zone');
+
+    // Verificar si todas las zonas de destino tienen una imagen colocada
+    dropZones.forEach(zone => {
+        if (zone.children.length === 0) {
+            allImagesPlaced = false;
+            return;
+        }
+    });
+
+    // Si todas las imágenes están colocadas, permitir que el usuario continúe
+    if (allImagesPlaced) {
+        // Ocultar el área de "Drag and Drop"
+        var dragAndDropContainer = document.getElementById('dragAndDropContainer');
+        dragAndDropContainer.style.display = 'none';
+
+        // Mostrar el modal con la información de la compra
+        showDetailsModal();
+    } else {
+        // Mostrar un mensaje de que las imágenes no se han verificado
+        alert('Por favor, coloca todas las imágenes en las zonas de destino antes de continuar.');
+    }
+});
+
+  </script>
 </body>
 @include('layouts.footer')
 

8b0e9230d0414cebbcb5c61699ca945c6d33db73
mohacr026-Portatil <mohamedcampusnet@gmail.com>
2024-04-17 13:38:16 +0200

    act

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -2,25 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use BaconQrCode\Renderer\Image\Png;
+use BaconQrCode\Writer;
 
 class QRController extends Controller
 {
-    public function generarQR($id_carrera, $id_jinete, $dorsal, $fechaHora)
+    public function generateQR()
     {
-        // Formatear la información para el QR
-        $informacion = "Carrera: $id_carrera\nJinete: $id_jinete\nDorsal: $dorsal\nFecha y Hora: $fechaHora";
+        // Datos para el código QR (por ejemplo, información de la inscripción)
+        $data = "Datos que quieres incluir en el código QR";
 
-        // Generar el código QR con la información
-        $qrUrl = route('nombre_de_la_ruta', [
-            'id_carrera' => $id_carrera,
-            'id_jinete' => $id_jinete,
-            'dorsal' => $dorsal,
-            'fechaHora' => $fechaHora
-        ]);
+        // Crea un objeto Writer
+        $writer = new Writer(new Png());
 
-        // Aquí deberías retornar la URL, no el código QR
-        return $qrUrl;
+        // Renderiza el código QR como un archivo PNG
+        $pngData = $writer->writeString($data);
+
+        // Devuelve el archivo PNG
+        return response($pngData)->header('Content-Type', 'image/png');
     }
 }
